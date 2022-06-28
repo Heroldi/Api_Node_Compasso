1ee4b613ed4b251c594a2b8de93d56ee1dc3c282
Daniel <danielkondlatsch.p@gmail.com>
2022-06-28 14:36:15 -0300

    Estilização e validação de todos campos na Register Task page

--- src/controllers/GetDadosTask.js
+++ src/controllers/GetDadosTask.js
@@ -1,31 +1,52 @@
 const wes = document.querySelector("#btn_task");
-wes.addEventListener("click", function(event) {
-    event.preventDefault();
+wes.addEventListener("click", function (event) {
+  event.preventDefault();
 });
-   
-    function GetDadosTask() {
-        var description =  document.querySelector('#description');
-        var date =  document.querySelector('#date');
-        var user =  document.querySelector('#user');
 
-        console.log(date.value + description.value + user.value);
-    
-        (async () => {
-            const rawResponse = await fetch('http://localhost:3000/api/v1/tasks', {
-              method: 'POST',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                "description" :  description.value,
-                "date" : date.value,
-                "user" : user.value,
-            })
-            });
-            const content = await rawResponse.json();
-          
-            console.log(content);
-          })();         
-        }
-          
+function GetDadosTask() {
+  if (validaData() == true) {
+    var description = document.querySelector("#description");
+    var date = document.querySelector("#date");
+    var user = document.querySelector("#user");
+
+    console.log(date.value + description.value + user.value);
+
+    (async () => {
+      const rawResponse = await fetch("http://localhost:3000/api/v1/tasks", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          description: description.value,
+          date: date.value,
+          user: user.value,
+        }),
+      });
+      const content = await rawResponse.json();
+
+      console.log(content);
+    })();
+    alert("Task successfully created");
+    clearInputs();
+  } else {
+    alert("Please insert a valid date");
+  }
+}
+// simple validation for date
+function validaData() {
+  var date = document.querySelector("#date");
+  // if date is greather than the current date alert "Data valida"
+  if (date.value > new Date().toISOString().split("T")[0]) {
+    return true;
+  }
+}
+
+// function that clear the inputs
+function clearInputs() {
+  var description = document.querySelector("#description");
+  var date = document.querySelector("#date");
+  description.value = "";
+  date.value = "";
+}
--- src/controllers/showUsers.js
+++ src/controllers/showUsers.js
@@ -1,41 +1,4 @@
 "use strict";
-// function showUser(id) {
-//     fetch("http://localhost:3000/api/v1/users/" + id)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//       });
-//   }
-// showUser('62b9f80a8ff56c5269fa756d');
-// create a function that will show all users
-// ------------------------------------------------------------------------------
-// function showUsers() {
-//     fetch("http://localhost:3000/api/v1/users")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//       });
-//   }
-// put each user data inside a <li> element
-// and put all <li> elements inside a <ul> element
-// and put the <ul> element inside the <body> element
-// function showUsers() {
-//     fetch("http://localhost:3000/api/v1/users")
-//         .then((response) => response.json())
-//         .then((data) => {
-//         let ul = document.createElement("ul");
-//         data.forEach((user) => {
-//             let li = document.createElement("li");
-//             li.innerHTML = user.name;
-//             ul.appendChild(li);
-//         });
-//         document.body.appendChild(ul);
-//         });
-//     }
-//     showUsers();
-// a function that will put each user data inside a <li> element
-// and put all <li> elements inside a <ul> element
-// and put the <ul> element inside the <body> element
 function showUsers() {
   fetch("http://localhost:3000/api/v1/users")
     .then((response) => response.json())
@@ -271,35 +234,3 @@ function showUsers() {
     });
 }
 showUsers();
-
-// update the user name with id "62bb09b5a899f33d5d727308" to "John Doe"
-function updateUser() {
-  let user = {
-    name: "John Doe",
-    cpf: "123456789",
-    birthdate: "01/01/2000",
-    email: "adsa@gmail.com",
-    password: "123456",
-    address: "Rua dos Bobos",
-    number: "123",
-    complement: "casa",
-    city: "São Paulo",
-    state: "SP",
-    country: "Brasil",
-    zipcode: "12345-678",
-  };
-  fetch("http://localhost:3000/api/v1/users/62bb09b5a899f33d5d727308", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-// updateUser();
